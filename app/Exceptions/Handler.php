d005f024073cbee59383729179b01c46518945f5
Andy Soell <andy@mettleup.com>
2017-05-02 12:11:03 +0200

    better error handling
    * Return JSON
    * Better message when requesting articles from an invalid source

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Response;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -44,7 +46,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof ModelNotFoundException) {
+            return Response::json(['message' => 'Entity not found'], 403);
+        } else {
+            return Response::json(['message' => 'server error'], 500);
+        }
     }
 
     /**
